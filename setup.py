5fa219394a6fc29cef1e85ec3244ec82999ca355
Feodor Fitsner <feodor@appveyor.com>
2025-04-22 19:04:21 -0700

    Try building for abi3

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import Extension, setup
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.10
+            return "cp310", "abi3", plat
+
+        return python, abi, plat
+
 
 setup(
     name="dart_bridge",
@@ -8,8 +21,9 @@ setup(
             "dart_bridge",
             sources=["src/dart_bridge.c", "src/dart_api/dart_api_dl.c"],
             include_dirs=["src/dart_api"],
-            define_macros=[("Py_LIMITED_API", "0x030C0000")],  # Target 3.12
+            define_macros=[("Py_LIMITED_API", "0x030A0000")],  # Target 3.10
             py_limited_api=True,
         )
     ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
 )
